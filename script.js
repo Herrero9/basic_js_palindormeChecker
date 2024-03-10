4b412ca283ed94eb6ab61187d3202f30fd3f5ddd
Sergio Herrero <serghebo@hotmail.es>
2024-03-10 21:49:03 +0100

    alert when no word input & various fixes in template and styles

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const palindromeForm = document.getElementById('palindromeForm');
-const submitButton = document.getElementById('submitButton');
+const inputText = document.getElementById('text-input');
+const submitButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
 
 const checkPalindrome = (e) => {
   e.preventDefault();
   let word = inputText.value;
-  console.log(word);
   let reverseWord = word.split('').reverse().join('');
-  console.log(reverseWord); 
-  if (word == reverseWord){
-    result.innerHTML = "This is a palindrome";
+  if(word !== null && word.length > 0){
+    if (word == reverseWord){
+      result.innerHTML = `${word} is a palindrome`;
+    }else{
+      result.innerHTML = `${word} is not a palindrome`;
+    }
   }else{
-    result.innerHTML = "This is not a palindrome";
+    alert("Please input a value");
   }
 }
 
