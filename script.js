a11b1de8849cd24e02cfeab26c79c849c7754a8e
Sergio Herrero <serghebo@hotmail.es>
2024-03-10 14:48:45 +0100

    check palindorme logic

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const checkPalindrome = (e) => {
   e.preventDefault();
   let word = inputText.value;
   console.log(word);
-    
+  let reverseWord = word.split('').reverse().join('');
+  console.log(reverseWord); 
+  if (word == reverseWord){
+    result.innerHTML = "This is a palindrome";
+  }else{
+    result.innerHTML = "This is not a palindrome";
+  }
 }
 
 palindromeForm.addEventListener("submit", checkPalindrome);
